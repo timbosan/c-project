9c4fcdefb8a4f4ad0ff2c78c09feadff2462d779
timbosan <timbosan@Timothys-iMac.local>
2020-06-17 19:13:59 -0400

    first

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PizzaParty
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("What is your name?  ");
+            string name = Console.ReadLine();
+
+            Console.Write("Hello " + name + " do you want to order a pizza?  Y/N  ");
+            string ordered = (Console.ReadLine()).ToUpper();
+            
+
+            if (ordered == "Y")
+            {
+                Console.Write("Awesome! I'll have you pick a time slot for Friday ");
+                //string time = Console.ReadLine();
+            }
+
+            if (ordered == "N");
+            {
+                Console.Write("Maybe next time, Have a great day!");
+            }
+
+        
+            
+        }
+   
+    
+    
+    }
+}
+
+
+
+    
+    
