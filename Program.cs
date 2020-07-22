92c0dab1b551ca2c67305d4b89e6f575066b42ea
timbosan <timbosan@Timothys-iMac.local>
2020-07-22 19:04:03 -0400

    added json and toppings loop

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace PizzaParty
 {
@@ -6,21 +8,28 @@ namespace PizzaParty
     {
         public static void Main()
         {
-          
+
             Console.Write("\nWhat is your name?  \n\nType 'EXIT' at anytime to exit the ordering app.\n>>");
             string name = Console.ReadLine();
-             //check each var at a read line for an exit
+            //check each var at a read line for an exit
             CheckForExit(name);
 
             Console.Write("Hello " + name + " do you want to order a pizza?  Y/N\n>>");
             string ordered = (Console.ReadLine()).ToUpper();
             CheckForExit(ordered);
-            
+
 
             if (ordered == "Y")
-            {   
-                timelist();
-                Toppings_Menu();
+            {
+                var CustomerTimeChoice = timelist();
+                var CustomerToppingChoices = Toppings_Menu();
+                var Overview = new PizzaOrder();
+                Overview.Name = name;
+                Overview.TimeSlot = CustomerTimeChoice;
+                Overview.Toppings = CustomerToppingChoices;
+                var Json = JsonConvert.SerializeObject(Overview);
+                System.IO.File.WriteAllText("order.Json", Json);
+                
             }
 
             else
@@ -28,48 +37,57 @@ namespace PizzaParty
                 Console.Write("\nMaybe next time, Have a great day!");
             }
 
-        
-            
-        }
-   
-        public static void timelist( )
-        {
-            string UserChoice;
-            string[] TimeSlots = {"6:00 - 6:15 ", "6:15 - 6:30", "6:30 - 6:45", "6:45 - 7:00"};
-            Console.Write("\nAwesome! I'll have you pick a time slot for Friday.\n\n\n");
-            Console.WriteLine("\nPlease select a time to pick up your Pizza\n");
-            for(int i = 0; i < TimeSlots.Length; i++)
-            {
-              int displayCounter = i+1;
-              Console.WriteLine('\n' + displayCounter.ToString() + ").  " + TimeSlots[i].ToString());
-            }
-            UserChoice = Console.ReadLine();
-                CheckForExit(UserChoice);
-            int UserChoiceasNUM = Int32.Parse(UserChoice);
-            Console.WriteLine("\nYou chose the time slot: " + TimeSlots[UserChoiceasNUM -1] + ". \n\n\n".ToString());
+
+
         }
 
-        public static void Toppings_Menu( )
+
+        // Functions
+        public static string timelist()
         {
-        string UserChoicetoppings;
-        string[] Toppings_Menu = {"Basil", "Hot Honey", "Italian Sausage", "Pepperoni", "Mushrooms", "Jalapenos", "Just Cheese"}; 
-        Console.WriteLine("\nPlease select toppings for your Pizza!");
-        for(int i = 0; i < Toppings_Menu.Length; i++)
+            string UserChoice;
+            string[] TimeSlots = { "6:00 - 6:15 ", "6:15 - 6:30", "6:30 - 6:45", "6:45 - 7:00" };
+            Console.Write("\nAwesome! I'll have you pick a time slot for Friday.\n\n\n");
+            Console.WriteLine("\nPlease select a time to pick up your Pizza\n");
+            for (int i = 0; i < TimeSlots.Length; i++)
             {
-              int displayCountertoppings = i+1;
-              Console.WriteLine('\n' + displayCountertoppings.ToString() + "). " + Toppings_Menu[i].ToString());
+                int displayCounter = i + 1;
+                Console.WriteLine('\n' + displayCounter.ToString() + ").  " + TimeSlots[i].ToString());
             }
-            UserChoicetoppings = Console.ReadLine();
+            UserChoice = Console.ReadLine();
+            CheckForExit(UserChoice);
+            int UserChoiceasNUM = Int32.Parse(UserChoice);
+            Console.WriteLine("\nYou chose the time slot: " + TimeSlots[UserChoiceasNUM - 1] + ". \n\n\n".ToString());
+            return TimeSlots[UserChoiceasNUM - 1];
+        }
+
+        public static List<string> Toppings_Menu()
+        {
+            List<string> CustomerToppingChoices = new List<string>();
+            string UserChoicetoppings;
+            string[] Toppings_Menu = { "Basil", "Hot Honey", "Italian Sausage", "Pepperoni", "Mushrooms", "Jalapenos", "Just Cheese", "Finished Adding Toppings" };
+            int toppingOption = 0;
+            while (toppingOption != 8)
+            {
+                Console.WriteLine("\nPlease select toppings for your Pizza!");
+                for (int i = 0; i < Toppings_Menu.Length; i++)
+                {
+                    int displayCountertoppings = i + 1;
+                    Console.WriteLine('\n' + displayCountertoppings.ToString() + "). " + Toppings_Menu[i].ToString());
+                }
+                UserChoicetoppings = Console.ReadLine();
                 CheckForExit(UserChoicetoppings);
-            int UserChoicetoppingsasNUM = Int32.Parse(UserChoicetoppings);
-            Console.WriteLine("\nYou picked these toppings: " + Toppings_Menu[UserChoicetoppingsasNUM -1].ToString());
-        
+                toppingOption = Int32.Parse(UserChoicetoppings);
+                CustomerToppingChoices.Add(Toppings_Menu[toppingOption - 1]);
+                Console.WriteLine("\nYou picked these toppings: " + Toppings_Menu[toppingOption - 1].ToString());
+            }
+            return CustomerToppingChoices;
         }
 
         // if user enters exit close program 
         public static void CheckForExit(string checker)
         {
-            if(checker.ToUpper() == "EXIT")
+            if (checker.ToUpper() == "EXIT")
             {
                 Environment.Exit(1);
             }
@@ -80,7 +98,13 @@ namespace PizzaParty
             }
         }
     }
-}    
+    public class PizzaOrder
+    {
+        public string Name{get;set;}
+        public List<string> Toppings{get;set;}
+        public string TimeSlot{get;set;}
+    }
+}
 
 
 
